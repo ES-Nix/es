edef779ebb6d4ed05e73b49b48a5116aa5d0b3d6
Pedro <pedroalencarregis@hotmail.com>
2023-04-06 09:09:55 -0300

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # es
+
+
+1)
+```bash
+command -v curl || (command -v apt && sudo apt-get update && sudo apt-get install -y curl)
+command -v curl || (command -v apk && sudo apk add --no-cache -y curl)
+
+
+NIX_RELEASE_VERSION=2.10.2 \
+&& curl -L https://releases.nixos.org/nix/nix-"${NIX_RELEASE_VERSION}"/install | sh -s -- --no-daemon \
+&& . "$HOME"/.nix-profile/etc/profile.d/nix.sh
+
+export NIX_CONFIG='extra-experimental-features = nix-command flakes'
+```
+
+
+
+
+```bash
+nix registry list
+```
+
+```bash
+nix flake show templates
+```
+
+
+TODO: test all the templates!
+```bash
+nix flake show templates --json
+```
+
+
+
+```bash
+mkdir -pv ~/sandbox/sandbox \
+&& cd ~/sandbox/sandbox
+```
+
+```bash
+nix flake init --template templates#full
+```
+
+```bash
+nix flake show .#
+```
+
+
+### 
+
+- nix flake init --template templates#full
+- https://xeiaso.net/blog/nix-flakes-terraform
+- https://juliu.is/tidying-your-home-with-nix/#ive-changed-my-mind-how-do-i-get-both-stable-and-unstable
+- https://discourse.nixos.org/t/home-manager-flake-does-not-provide-attribute/24926
+- https://discourse.nixos.org/t/fixing-error-attribute-currentsystem-missing-in-flake/22386/7
+
+
+
+
+
+git init && git add .
+
+
+nix --extra-experimental-features 'nix-command flakes' build -L --rebuild .#hello
+
+checks.suportedSystem = self.packages.suportedSystem;
+
+
+mkdir -pv hosts/minimal-example-nixos
+
---
+++ src/templates/start-slim-config/flake.nix
@@ -0,0 +1,103 @@
+{
+  inputs = {
+
+    nixpkgs-darwin-stable.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
+    nixpkgs-linux-stable.url = "github:nixos/nixpkgs/nixos-22.11";
+
+    nixpkgs-linux-unstable.url = "nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-linux-stable";
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs-linux-stable";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs-linux-stable";
+
+  };
+
+  outputs = {
+    self, 
+    nixpkgs-linux-stable,
+    nixpkgs-linux-unstable, 
+    nixpkgs-darwin-stable, 
+    flake-utils, 
+    nixos-generators,
+    home-manager,
+  }:
+    let
+
+      suportedSystems = [ 
+        "x86_64-linux" 
+        # "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      mkSystem = extraModules:
+        nixpkgs-linux-stable.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          modules = [
+            # bake the git revision of the repo into the system
+            ({ ... }: { system.configurationRevision = self.sourceInfo.rev; })
+          ] ++ extraModules;
+        };
+
+        f = { system, username, arg-nixpkgs, home ? "", stateVersion ? "22.11" }:
+            let
+              pkgs = "${arg-nixpkgs}".legacyPackages."${system}";
+            in
+            home-manager.lib.homeManagerConfiguration {
+              modules = [
+                {
+                  home = {
+                    username = "${username}";
+                    homeDirectory = "/home/" + "${username}";
+                    stateVersion = "${stateVersion}";
+                    packages = with pkgs; [
+                      sl
+                      cowsay
+                    ];
+                  };
+
+                  programs.home-manager.enable = true;
+
+                }
+
+                "${home}"
+                
+              ];
+
+              # TODO: how to: Optionally use extraSpecialArgs
+              # to pass through arguments to home.nix
+              # extraSpecialArgs = { pkgs-unstable = pkgs; };
+            };
+
+    in flake-utils.lib.eachSystem suportedSystems (suportedSystem:
+      let pkgs = import nixpkgs-linux-stable { system = suportedSystem; };
+      in rec {
+        devShells.default =
+          pkgs.mkShell { buildInputs = with pkgs; [ bashInteractive ]; };
+      } // {
+        # TODO: put nixosConfigurations here later
+
+        checks."${suportedSystem}" = self.packages."${suportedSystem}".hello;
+
+        packages.hello = pkgs.hello;
+        packages.python3WithPandas = pkgs.python3Packages.pandas;
+
+        apps.hello = {
+          type = "app";
+          program = self.packages."${suportedSystem}".hello;
+        };
+
+        homeConfigurations = {
+
+          "vagrant-alpine316.localdomain" = f { system = "${suportedSystem}"; username = "vagrant"; };
+          "ubuntu-ubuntu2204-ec2" = f { system = "${suportedSystem}"; username = "ubuntu"; };
+        };
+
+      });
+}
