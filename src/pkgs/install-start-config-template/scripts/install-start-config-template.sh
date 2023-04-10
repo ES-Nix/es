d675fb4a56857e7ee835add8855e20939c4651e1
Pedro <pedroalencarregis@hotmail.com>
2023-04-09 21:59:46 -0300

    Tests: apps, fix quotes

--- src/pkgs/install-start-config-template/scripts/install-start-config-template.sh
+++ src/pkgs/install-start-config-template/scripts/install-start-config-template.sh
@@ -75,7 +75,7 @@ echo hm
 
 # && time home-manager switch -b backuphm --impure --flake "$DIRECTORY_TO_CLONE"#$HM_ATTR_FULL_NAME \
 export NIXPKGS_ALLOW_UNFREE=1 \
-&& time home-manager switch -b backuphm --impure --flake "$DIRECTORY_TO_CLONE"#$(nix eval --impure --raw --expr 'builtins.currentSystem').$DUMMY_USER-$DUMMY_HOSTNAME \
+&& time home-manager switch -b backuphm --impure --flake "$DIRECTORY_TO_CLONE"#$DUMMY_USER-$DUMMY_HOSTNAME \
 && home-manager generations
 
 
