11076599ee66411035f900b6cc482236de0406fc
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2023-09-19 23:55:44 -0300

    Testing template, drop nerdfonts

--- src/templates/start-config/home.nix
+++ src/templates/start-config/home.nix
@@ -143,7 +143,7 @@
     # lineicons
     # montserrat
     # mplus-outline-fonts
-    nerdfonts # Really big, but only with this font some issues with starship were fixed.
+    # nerdfonts # Really big, but only with this font some issues with starship were fixed.
     # noto-fonts
     # noto-fonts-emoji
     # noto-fonts-extra
