6d8f2737cda97ec2d61e144726a1bc4a7acb0162
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2024-01-05 12:57:11 -0300

    Testing new templates, WIP, nixuser

--- src/templates/qemu-virtual-machine-xfce-copy-paste-minimal/flake.nix
+++ src/templates/qemu-virtual-machine-xfce-copy-paste-minimal/flake.nix
@@ -133,7 +133,7 @@
               services.xserver.layout = "br";
               services.xserver.desktopManager.xfce.enable = true;
               services.xserver.desktopManager.xfce.enableScreensaver = false;
-              services.xserver.displayManager.autoLogin.user = "nixos";
+              services.xserver.displayManager.autoLogin.user = "nixuser";
               services.xserver.displayManager.sessionCommands = ''
                 exo-open \
                   --launch TerminalEmulator \
