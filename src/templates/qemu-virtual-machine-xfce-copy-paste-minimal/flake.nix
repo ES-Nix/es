670bb8ba4af55eed102bba413aa7103f534f8cd5
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2024-01-08 17:57:17 -0300

    WIP: only nix run??

--- src/templates/qemu-virtual-machine-xfce-copy-paste-minimal/flake.nix
+++ src/templates/qemu-virtual-machine-xfce-copy-paste-minimal/flake.nix
@@ -124,6 +124,7 @@
                     "-device virtio-serial"
                     "-chardev spicevmc,id=vdagent,debug=0,name=vdagent"
                     "-device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+                    "-daemonize"
                   ];
                 };
 
