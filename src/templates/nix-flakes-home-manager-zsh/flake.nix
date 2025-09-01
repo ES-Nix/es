c8113b90edfd0938046136dff83eecc967ab0b8a
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2025-08-31 21:37:19 -0300

    W

--- src/templates/nix-flakes-home-manager-zsh/flake.nix
+++ src/templates/nix-flakes-home-manager-zsh/flake.nix
@@ -37,8 +37,283 @@
       overlays.default = nixpkgs.lib.composeManyExtensions [
         (final: prev: {
           fooBar = prev.hello;
+
+          nixos-vm = nixpkgs.lib.nixosSystem {
+            system = prev.system;
+            modules = [
+              ({ config, nixpkgs, pkgs, lib, modulesPath, ... }:
+                {
+                  # Internationalisation options
+                  i18n.defaultLocale = "en_US.UTF-8";
+                  console.keyMap = "br-abnt2";
+
+                  # Set your time zone.
+                  time.timeZone = "America/Recife";
+
+                  # Why
+                  # nix flake show --impure .#
+                  # break if it does not exists?
+                  # Use systemd boot (EFI only)
+                  boot.loader.systemd-boot.enable = true;
+                  fileSystems."/" = { device = "/dev/hda1"; };
+
+                  virtualisation.vmVariant =
+                    {
+                      virtualisation.docker.enable = true;
+                      virtualisation.podman.enable = true;
+
+                      virtualisation.memorySize = 1024 * 9; # Use MiB memory.
+                      virtualisation.diskSize = 1024 * 50; # Use MiB memory.
+                      virtualisation.cores = 7; # Number of cores.
+                      virtualisation.graphics = true;
+
+                      virtualisation.resolution = lib.mkForce { x = 1024; y = 768; };
+
+                      virtualisation.qemu.options = [
+                        # https://www.spice-space.org/spice-user-manual.html#Running_qemu_manually
+                        # remote-viewer spice://localhost:3001
+
+                        # "-daemonize" # How to save the QEMU PID?
+                        "-machine vmport=off"
+                        "-vga qxl"
+                        "-spice port=3001,disable-ticketing=on"
+                        "-device virtio-serial"
+                        "-chardev spicevmc,id=vdagent,debug=0,name=vdagent"
+                        "-device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+                      ];
+
+                      virtualisation.useNixStoreImage = false; # TODO: hardening
+                      virtualisation.writableStore = true; # TODO: hardening
+                    };
+                  security.sudo.wheelNeedsPassword = false; # TODO: hardening
+                  # https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
+                  users.extraGroups.nixgroup.gid = 999;
+                  users.users.nixuser = {
+                    isSystemUser = true;
+                    password = "1"; # TODO: hardening
+                    createHome = true;
+                    home = "/home/nixuser";
+                    homeMode = "0700";
+                    description = "The VM tester user";
+                    group = "nixgroup";
+                    extraGroups = [
+                      "docker"
+                      "wheel"
+                    ];
+                    packages = with pkgs; [
+                      file
+                      firefox
+                      git
+                      jq
+                      lsof
+                      findutils
+                      dive
+                      fooBar
+                      starship
+                      direnv
+                      fzf
+                      sudo
+                      which
+                    ];
+                    shell = pkgs.zsh;
+                    uid = 1234;
+                    autoSubUidGidRange = true;
+                  };
+
+                  # https://github.com/NixOS/nixpkgs/blob/3a44e0112836b777b176870bb44155a2c1dbc226/nixos/modules/programs/zsh/oh-my-zsh.nix#L119
+                  # https://discourse.nixos.org/t/nix-completions-for-zsh/5532
+                  # https://github.com/NixOS/nixpkgs/blob/09aa1b23bb5f04dfc0ac306a379a464584fc8de7/nixos/modules/programs/zsh/zsh.nix#L230-L231
+                  programs.zsh = {
+                    enable = true;
+                    shellAliases = {
+                      vim = "nvim";
+                    };
+
+                    enableCompletion = true;
+                    autosuggestions.enable = true;
+                    syntaxHighlighting.enable = true;
+                    interactiveShellInit = ''
+                      export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
+                      export ZSH_THEME="agnoster"
+                      export ZSH_CUSTOM=${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions
+                      plugins=(
+                                colored-man-pages
+                                docker
+                                git
+                                #zsh-autosuggestions # Why this causes an warn?
+                                #zsh-syntax-highlighting
+                              )
+
+                      # https://nixos.wiki/wiki/Fzf
+                      source $ZSH/oh-my-zsh.sh
+
+                      export DIRENV_LOG_FORMAT=""
+                      eval "$(direnv hook zsh)"
+
+                      eval "$(starship init zsh)"
+
+                      export FZF_BASE=$(fzf-share)
+                      source "$(fzf-share)/completion.zsh"
+                      source "$(fzf-share)/key-bindings.zsh"
+                    '';
+
+                    ohMyZsh.custom = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+                    promptInit = "";
+                  };
+
+                  fonts = {
+                    fontDir.enable = true;
+                    packages = with pkgs; [
+                      powerline
+                      powerline-fonts
+                    ];
+                    enableDefaultPackages = true;
+                    enableGhostscriptFonts = true;
+                  };
+
+                  # Hack to fix annoying zsh warning, too overkill probably
+                  # https://www.reddit.com/r/NixOS/comments/cg102t/how_to_run_a_shell_command_upon_startup/eudvtz1/?utm_source=reddit&utm_medium=web2x&context=3
+                  # https://stackoverflow.com/questions/638975/how-wdo-i-tell-if-a-regular-file-does-not-exist-in-bash#comment25226870_638985
+                  systemd.user.services.fix-zsh-warning = {
+                    script = ''
+                      test -f /home/nixuser/.zshrc || touch /home/nixuser/.zshrc && chown nixuser: -Rv /home/nixuser
+                    '';
+                    wantedBy = [ "default.target" ];
+                  };
+
+                  services.xserver.enable = true;
+                  services.xserver.xkb.layout = "br";
+                  services.displayManager.autoLogin.user = "nixuser";
+                  services.xserver.displayManager.sessionCommands = ''
+                    exo-open \
+                      --launch TerminalEmulator \
+                      --zoom=-3 \
+                      --geometry 154x40
+                  '';
+
+                  # https://nixos.org/manual/nixos/stable/#sec-xfce
+                  services.xserver.desktopManager.xfce.enable = true;
+                  services.xserver.desktopManager.xfce.enableScreensaver = false;
+                  services.xserver.videoDrivers = [ "qxl" ];
+                  services.spice-vdagentd.enable = true; # For copy/paste to work
+
+                  nix.extraOptions = "experimental-features = nix-command flakes";
+
+                  environment.systemPackages = with pkgs; [
+                  ];
+
+                  system.stateVersion = "25.05";
+                })
+
+              { nixpkgs.overlays = [ overlays.default ]; }
+            ];
+            specialArgs = { inherit nixpkgs; };
+          };
+
+          myvm = final.nixos-vm.config.system.build.vm;
+
+          automatic-vm = prev.writeShellApplication {
+            name = "run-nixos-vm";
+            runtimeInputs = with final; [ curl virt-viewer ];
+            text = ''
+              export VNC_PORT=3001
+
+              ${final.lib.getExe final.myvm} & PID_QEMU="$!"
+
+              for _ in {0..50}; do
+                if [[ $(curl --fail --silent http://localhost:"$VNC_PORT") -eq 1 ]];
+                then
+                  break
+                fi
+                # date +'%d/%m/%Y %H:%M:%S:%3N'
+                sleep 0.1
+              done;
+
+              remote-viewer spice://localhost:"$VNC_PORT"
+
+              kill $PID_QEMU
+            '';
+          };
+
+          hm = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              ({ pkgs, ... }:
+                {
+                  home.stateVersion = "25.05";
+                  home.username = "${userName}";
+                  home.homeDirectory = "${homeDirectory}";
+
+                  programs.home-manager = {
+                    enable = true;
+                  };
+
+                  home.packages = with pkgs; [
+                    direnv
+                    git
+                    nix
+                    zsh
+                    fooBar
+                    automatic-vm
+                  ];
+
+                  nix = {
+                    enable = true;
+                    package = pkgs.nix; # pkgs.nixVersions.latest
+                    extraOptions = ''
+                      experimental-features = nix-command flakes
+                    '';
+                    registry.nixpkgs.flake = nixpkgs;
+                    settings = {
+                      bash-prompt-prefix = "(nix:$name)\\040";
+                      keep-derivations = true;
+                      keep-env-derivations = true;
+                      keep-failed = true;
+                      keep-going = true;
+                      keep-outputs = true;
+                      nix-path = "nixpkgs=flake:nixpkgs";
+                      tarball-ttl = 2419200; # 60 * 60 * 24 * 7 * 4 = one month
+                    };
+                  };
+
+                  programs.zsh = {
+                    enable = true;
+                    enableCompletion = true;
+                    dotDir = ".config/zsh";
+                    autosuggestion.enable = true;
+                    syntaxHighlighting.enable = true;
+                    envExtra = ''
+                      if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+                        . ~/.nix-profile/etc/profile.d/nix.sh
+                      fi
+                    '';
+                    shellAliases = {
+                      l = "ls -alh";
+                    };
+                    sessionVariables = {
+                      # https://discourse.nixos.org/t/what-is-the-correct-way-to-set-nix-path-with-home-manager-on-ubuntu/29736
+                      # NIX_PATH = "nixpkgs=${pkgs.path}";
+                      LANG = "en_US.utf8";
+                    };
+                    oh-my-zsh = {
+                      enable = true;
+                      plugins = [
+                        "colored-man-pages"
+                        "colorize"
+                        "direnv"
+                        "zsh-navigation-tools"
+                      ];
+                      theme = "robbyrussell";
+                    };
+                  };
+                }
+              )
+            ];
+            extraSpecialArgs = { nixpkgs = nixpkgs; };
+          };
         })
       ];
+
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ overlays.default ];
@@ -56,82 +331,25 @@
         '';
       };
 
-      packages."${system}".default = self.homeConfigurations."${userName}".activationPackage;
-
-      homeConfigurations."${userName}" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ({ pkgs, ... }:
-            {
-              home.stateVersion = "25.05";
-              home.username = "${userName}";
-              home.homeDirectory = "${homeDirectory}";
-
-              programs.home-manager = {
-                enable = true;
-              };
-
-              home.packages = with pkgs; [
-                direnv
-                git
-                nix
-                zsh
-                fooBar
-              ];
-
-              nix = {
-                enable = true;
-                package = pkgs.nix; # pkgs.nixVersions.latest
-                extraOptions = ''
-                  experimental-features = nix-command flakes
-                '';
-                registry.nixpkgs.flake = nixpkgs;
-                settings = {
-                  bash-prompt-prefix = "(nix:$name)\\040";
-                  keep-derivations = true;
-                  keep-env-derivations = true;
-                  keep-failed = true;
-                  keep-going = true;
-                  keep-outputs = true;
-                  nix-path = "nixpkgs=flake:nixpkgs";
-                  tarball-ttl = 2419200; # 60 * 60 * 24 * 7 * 4 = one month
-                };
-              };
-
-              programs.zsh = {
-                enable = true;
-                enableCompletion = true;
-                dotDir = ".config/zsh";
-                autosuggestion.enable = true;
-                syntaxHighlighting.enable = true;
-                envExtra = ''
-                  if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
-                    . ~/.nix-profile/etc/profile.d/nix.sh
-                  fi
-                '';
-                shellAliases = {
-                  l = "ls -alh";
-                };
-                sessionVariables = {
-                  # https://discourse.nixos.org/t/what-is-the-correct-way-to-set-nix-path-with-home-manager-on-ubuntu/29736
-                  # NIX_PATH = "nixpkgs=${pkgs.path}";
-                  LANG = "en_US.utf8";
-                };
-                oh-my-zsh = {
-                  enable = true;
-                  plugins = [
-                    "colored-man-pages"
-                    "colorize"
-                    "direnv"
-                    "zsh-navigation-tools"
-                  ];
-                  theme = "robbyrussell";
-                };
-              };
-            }
-          )
-        ];
-        extraSpecialArgs = { nixpkgs = nixpkgs; };
+      packages."${system}" = {
+        default = self.homeConfigurations."${userName}".activationPackage;
+        automatic-vm = pkgs.automatic-vm;
       };
+      checks."${system}" = {
+        inherit (pkgs)
+          fooBar
+          ;
+        default = pkgs.fooBar;
+        aaaa = self.homeConfigurations."${userName}".activationPackage;
+      };
+      apps."${system}" = {
+        automatic-vm = {
+          type = "app";
+          program = "${pkgs.lib.getExe pkgs.automatic-vm}";
+          meta.description = "Run the automatic-vm";
+        };
+      };
+
+      homeConfigurations."${userName}" = pkgs.hm;
     };
 }
