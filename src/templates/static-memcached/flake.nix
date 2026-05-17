6032d8faf2f356aa61e1702a0212adb6d67c5ab6
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2026-05-17 14:42:06 -0300

    wip: fixing mult architecture

--- src/templates/static-memcached/flake.nix
+++ src/templates/static-memcached/flake.nix
@@ -304,7 +304,7 @@
       # nix flake show --allow-import-from-derivation --impure --refresh .#
       suportedSystems = [
         "x86_64-linux"
-        # "aarch64-linux"
+        "aarch64-linux"
         # "aarch64-darwin"
         # "x86_64-darwin"
       ];
