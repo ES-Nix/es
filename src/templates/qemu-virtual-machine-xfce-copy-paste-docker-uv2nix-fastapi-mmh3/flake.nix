9d53608605f224d763afd383a1858a44336370c9
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2026-06-10 18:55:36 -0300

    feat: migrate from poetry2nix to uv2nix

--- src/templates/qemu-virtual-machine-xfce-copy-paste-docker-uv2nix-fastapi-mmh3/flake.nix
+++ src/templates/qemu-virtual-machine-xfce-copy-paste-docker-uv2nix-fastapi-mmh3/flake.nix
@@ -1,78 +1,47 @@
 {
-  description = "A QEMU virtual machine with XFCE, copy/paste, Docker, poetry2nix, FastAPI, mmh3";
+  description = "A QEMU virtual machine with XFCE, copy/paste, Docker, uv2nix, FastAPI, mmh3";
 
-  /*
-    nix \
-    flake \
-    lock \
-    --override-input nixpkgs 'github:NixOS/nixpkgs/d063c1dd113c91ab27959ba540c0d9753409edf3' \
-    --override-input flake-utils 'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a' \
-    --override-input poetry2nix 'github:nix-community/poetry2nix/3c92540611f42d3fb2d0d084a6c694cd6544b609'
-
-    nix \
-    flake \
-    lock \
-    --override-input nixpkgs 'github:NixOS/nixpkgs/057f63b6dc1a2c67301286152eb5af20747a9cb4' \
-    --override-input flake-utils 'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a' \
-    --override-input poetry2nix 'github:nix-community/poetry2nix/f554d27c1544d9c56e5f1f8e2b8aff399803674e'
-
-    nix \
-    flake \
-    lock \
-    --override-input nixpkgs 'github:NixOS/nixpkgs/11415c7ae8539d6292f2928317ee7a8410b28bb9' \
-    --override-input flake-utils 'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a' \
-    --override-input poetry2nix 'github:nix-community/poetry2nix/f554d27c1544d9c56e5f1f8e2b8aff399803674e'
-      
-    nix \
-    flake \
-    lock \
-    --override-input nixpkgs 'github:NixOS/nixpkgs/cdd2ef009676ac92b715ff26630164bb88fec4e0' \
-    --override-input flake-utils 'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b' \
-    --override-input poetry2nix 'github:nix-community/poetry2nix/b9a98080beff0903a5e5fe431f42cde1e3e50d6b'  
-
-    nix \
-    flake \
-    lock \
-    --override-input nixpkgs 'github:NixOS/nixpkgs/fd487183437963a59ba763c0cc4f27e3447dd6dd' \
-    --override-input flake-utils 'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b' \
-    --override-input poetry2nix 'github:nix-community/poetry2nix/b9a98080beff0903a5e5fe431f42cde1e3e50d6b'
-
-    25.11
-    nix \
-    flake \
-    lock \
-    --override-input nixpkgs 'github:NixOS/nixpkgs/f560ccec6b1116b22e6ed15f4c510997d99d5852' \
-    --override-input flake-utils 'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b' \
-    --override-input poetry2nix 'github:nix-community/poetry2nix/b9a98080beff0903a5e5fe431f42cde1e3e50d6b'
-
-  */
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
+  outputs = { self, nixpkgs, flake-utils, pyproject-nix, uv2nix, pyproject-build-systems }: {
     overlays.default = nixpkgs.lib.composeManyExtensions [
       (final: prev: {
         fooBar = prev.hello;
 
-        p2n = poetry2nix.lib.mkPoetry2Nix { pkgs = prev; };
-        myapp = final.p2n.mkPoetryApplication
-          {
-            projectDir = final.p2n.cleanPythonSources { src = ./.; };
-            preferWheels = true;
-            python = final.python311;
-
-            overrides = final.p2n.defaultPoetryOverrides.extend
-              (final: prev: {
-                # 
-              });
-          } // { meta.mainProgram = builtins.head (builtins.attrNames (builtins.fromTOML (builtins.readFile ./pyproject.toml)).tool.poetry.scripts); };
+        myapp =
+          let
+            python = prev.python311;
+            workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+            overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
+            pythonSet = (prev.callPackage pyproject-nix.lib.renderers.withPackages {
+              inherit python;
+            }).override (old: {
+              overlays = old.overlays ++ [
+                overlay
+                pyproject-build-systems.overlays.default
+              ];
+            });
+          in
+          (pythonSet.mkVirtualEnv "myapp-env" workspace.deps.default)
+          // { meta.mainProgram = "start-app"; };
 
         myappOCIImage =
           let
@@ -464,26 +433,21 @@
         devShells.default = with pkgs; mkShell {
           packages = [
             python311
-            poetry
+            uv
             fooBar
-            # myapp
-            # myappOCIImage
-            # testMyappOCIImage
-            # automaticVm
           ];
+
+          env = {
+            UV_PYTHON = python311.interpreter;
+            UV_PYTHON_DOWNLOADS = "never";
+          };
 
           shellHook = ''
             test -d .profiles || mkdir -v .profiles
             test -L .profiles/dev \
-            || nix develop --impure .# --profile .profiles/dev --command true             
+            || nix develop --impure .# --profile .profiles/dev --command true
+            unset PYTHONPATH
           '';
-        };
-
-        # Shell for poetry.
-        #     nix develop .#poetry
-        # Use this shell for changes to pyproject.toml and poetry.lock.
-        devShells.poetry = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
         };
       }
     )
